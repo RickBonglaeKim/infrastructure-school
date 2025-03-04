985ed2278b9a9425d4bb67718f9b955c4c5dc21f
Iscream-Rick <rick.kim@i-screamarts.com>
2025-03-04 14:15:31 +0900

    Updated listener for the env-prod.

--- module/load-balancer/lb.listener.rule.prod.tf
+++ module/load-balancer/lb.listener.rule.prod.tf
@@ -1,15 +1,15 @@
 
-resource "aws_lb_listener_rule" "school-ver3-client" {
-  count = var.environment == "prod" ? 1 : 0    // Will be configured in dev environment.
+resource "aws_lb_listener_rule" "school-ver3-client-prod" {
+  count = var.environment == "prod" ? 1 : 0    // Will be configured in prod environment.
 
   listener_arn = aws_lb_listener.school-ver3-https.arn
   priority = 101
 
   condition {
     host_header {
-      values = [ "school-server.art-bonbon.com" ]
+      values = [ "school-server-bonbon.com" ]
     }
-  }  
+  }
   action {
     type = "forward"
     target_group_arn = aws_lb_target_group.school-ver3-client.arn
@@ -17,15 +17,15 @@ resource "aws_lb_listener_rule" "school-ver3-client" {
 }
 
 
-resource "aws_lb_listener_rule" "school-ver3-teacher" {
-  count = var.environment == "prod" ? 1 : 0    // Will be configured in dev environment.
+resource "aws_lb_listener_rule" "school-ver3-teacher-preview-prod" {
+  count = var.environment == "prod" ? 1 : 0    // Will be configured in prod environment.
 
   listener_arn = aws_lb_listener.school-ver3-https.arn
-  priority = 102
+  priority = 901
 
   condition {
     host_header {
-      values = [ "school-teacher.dev.art-bonbon.com" ]
+      values = [ "school-teacher-preview.art-bonbon.com" ]
     }
   }
 
@@ -36,15 +36,34 @@ resource "aws_lb_listener_rule" "school-ver3-teacher" {
 }
 
 
-resource "aws_lb_listener_rule" "school-ver3-student" {
-  count = var.environment == "prod" ? 1 : 0    // Will be configured in dev environment.
+resource "aws_lb_listener_rule" "school-ver3-teacher-prod" {
+  count = var.environment == "prod" ? 1 : 0    // Will be configured in prod environment.
 
   listener_arn = aws_lb_listener.school-ver3-https.arn
   priority = 201
 
   condition {
     host_header {
-      values = ["school-student.dev.art-bonbon.com"]
+      values = [ "school-teacher.art-bonbon.com" ]
+    }
+  }
+
+  action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.school-ver3-teacher.arn
+  }
+}
+
+
+resource "aws_lb_listener_rule" "school-ver3-student-prod" {
+  count = var.environment == "prod" ? 1 : 0    // Will be configured in prod environment.
+
+  listener_arn = aws_lb_listener.school-ver3-https.arn
+  priority = 202
+
+  condition {
+    host_header {
+      values = ["school-student.art-bonbon.com"]
     }
   }
 
